e5cd01471e502d3baecca9c3924b407b56c78658
Nikita Astaforkin <astaforkin90@gmail.com>
2022-08-15 11:24:32 +0300

    refactoring task1

--- stepikPythonPro.py
+++ stepikPythonPro.py
@@ -21,13 +21,8 @@
 def task1():
     import math
     a, b = int(input()), int(input())
-    print(a + b)
-    print(a - b)
-    print(a * b)
-    print(a / b)
-    print(a//b)
-    print(a % b)
-    print(math.sqrt(a**10 + b**10))
+    
+    print(a + b, a - b, a * b, a / b, a // b, a % b, (a**10 + b**10)**0.5, sep='\n')
 
 
 
